13e776089b1d5436b4221062c41e1aec2928987b
eesh <eesh@mit.edu>
2020-09-10 13:07:49 -0400

    added ITP code + better color scheme

--- make_env_plots_3x3.m
+++ make_env_plots_3x3.m
@@ -5,13 +5,12 @@
 %% prep workspace
 clear; clc;
 
-%% load data
+%% load CTD data
 
-% load Bradli's CTDs
 load('~/.dropboxmit/icex_2020_mat/bradli_casts_cleaned_ecb.mat');
 request_lat = 71.183; request_lon = -142.405 + 360;
 
-% prep HYCOM
+%% prep HYCOM
 hycom_file_path = '~/.dropboxmit/icex_2020_mat/hycom-files/ts3z-hindcast-Mar3-Mar13.nc4';
 hycom_lon = ncread(hycom_file_path,'lon');
 hycom_lat = ncread(hycom_file_path,'lat');
@@ -25,14 +24,37 @@ for tt = 1:length(hycom_time)
 end
 clear tt;
 
+%% prep ITPs --- ITP103 and ITP119
 
-%% make transect style plot
-% transect(x,t,v) --- x = array, d = cell array, v = cell array
+listing = dir('~/.dropboxmit/icex_2020_mat/itp-files/itp*grd*.dat');
 
+itp_search.filepath = strings(length(listing),1);
+itp_search.lon = zeros(length(listing),1);
+itp_search.lat = zeros(length(listing),1);
+itp_search.time = zeros(length(listing),1);
+
+% make list of locations, times, and paths
+for cc = 1:length(listing)
+    dat_file_path = fullfile(listing(cc).folder, listing(cc).name);
+    
+    itp_search.filepath(cc)   = dat_file_path;
+    info                        = itp_import_loc(dat_file_path);
+    
+    itp_search.lon(cc)        = info.lon;
+    itp_search.lat(cc)        = info.lat;
+    itp_search.time(cc)       = datenum([info.year 0 info.day 0 0 0]);
+end
+
+num_check = 3;
+itp_c = cell(length(CTD),num_check);
+itp_z = cell(length(CTD),num_check);
+
+
+%% loop through CTDs
 for p = 1:length(CTD)
     
     %% ctd info
-    ind_below = find(CTD(p).raw_z > 1);
+    ind_below = find(CTD(p).raw_z > 2);
     
     ctd_z{p} = CTD(p).raw_z(ind_below);
     ctd_c{p} = CTD(p).raw_c(ind_below);
@@ -41,7 +63,7 @@ for p = 1:length(CTD)
     request_depth = max(CTD(p).raw_z);
     request_time = datenum(ctd_t(p));
     
-    %% hycom info    
+    %% hycom info
     idx_lat = find_start(hycom_lat,request_lat);
     idx_lon = find_start(hycom_lon,request_lon);
     idx_z = find_start(hycom_depth,request_depth);
@@ -79,7 +101,41 @@ for p = 1:length(CTD)
     hycom_z{p} = squeeze(zmat(1,1,:,1));
     hycom_c{p} = Cq;
     
+    %% itp info
+    
+    % pick "nearest neighbor" by z-score
+    
+    z_lat = abs((itp_search.lat - request_lat)./std(itp_search.lat));
+    z_lon = abs((itp_search.lon - (request_lon - 360))./std(itp_search.lon));
+    z_time = abs((itp_search.time - request_time)./std(itp_search.time));
+    
+    z_total = 5.*z_time + z_lat + z_lon;
+    [B,best_index] = sort(z_total,'ascend');
+    
+    count = 0;
+    for lp = 1:num_check
+        num_obs = 0;
+        while (num_obs < 200)
+            count = count + 1;
+            [~,~,pressure,temp,salinity,~] = importfile_itp(itp_search.filepath(best_index(count)));
+            num_obs = length(pressure);
+            
+            itp_z{p,lp} = pressureToDepth(pressure,itp_search.lat(best_index(count)));
+            itp_c{p,lp} = snd_spd(pressure,temp,salinity,'chen');
+            
+            best_itp_lat(p,lp) = itp_search.lat(best_index(count));
+            best_itp_lon(p,lp) = itp_search.lon(best_index(count));
+            best_itp_time(p,lp) = itp_search.time(best_index(count));
+            best_itp_filename(p,lp) = itp_search.filepath(best_index(count));
+        end
+    end
+    
+    
+    
+    
 end
+
+%% write to a figure
 
 figure(1); clf;
 ha = tight_subplot(3,3,[.06 .03],[.1 .05],[.1 .02]);
@@ -89,17 +145,18 @@ for p = 1:9
     % get figure
     axes(ha(p));
     
-    % itp color -- [5 119 177]./256;
-    
-    if strcmp(CTD(p).type,'rsk');
+    if strcmp(CTD(p).type,'rsk')
         color = [153 51 153]./256;
-    elseif strcmp(CTD(p).type,'xctd');
+    elseif strcmp(CTD(p).type,'xctd')
         color = [200 87 0]./256;
     end
     
-    p1 = plot(hycom_c{p},hycom_z{p},'color',[181 181 181]./256,'linewidth',3);
+    p_hycom = plot(hycom_c{p},hycom_z{p},'color',[181 181 181 220]./256,'linewidth',3);
     hold on
-    p2 = plot(ctd_c{p},ctd_z{p},'color',color,'linewidth',3);
+    for lp = 1:num_check
+        p_itp = plot(itp_c{p,lp},itp_z{p,lp},'color',[5 119 177 80]./256,'linewidth',3);
+    end
+    p_ctd = plot(ctd_c{p},ctd_z{p},'-','color',color,'linewidth',3);
     hold off
     
     beautify_plot();
@@ -122,9 +179,9 @@ for p = 1:9
     end
     
     if p == 1
-        legend([p2 p1],'RSK','HYCOM','location','best')
+        legend([p_ctd p_hycom p_itp],'RSK','HYCOM','ITP','location','best')
     elseif p==5
-        legend([p2 p1],'xCTD','HYCOM','location','best');
+        legend([p_ctd p_hycom p_itp],'xCTD','HYCOM','ITP','location','best');
     end
 end
 
