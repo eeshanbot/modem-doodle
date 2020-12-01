41e7e6e00c8d56fc94bfa10575ba1318ff3b680b
eesh <eesh@mit.edu>
2020-12-01 16:57:21 -0500

    sorts through toby test and gvel data successfully

--- src/main_collate_toby_test_data.m
+++ src/main_collate_toby_test_data.m
@@ -2,18 +2,29 @@
 
 % runs through each toby_test file and creates new working files by event
 % outputs a new mat file for each toby test:
-% tx: x,y,z,lat,lon,name,id,time
-% rx: x,y,z,lat,lon,name,id,time
-% info: range, gvel, gvelstd
+% 
+% tag       owtt, src, dest, rec, name, gvelNode, tstr, time
+% tx        id, name, x, y, z, lat, lon, depth, time
+% rx        id, name, x, y, z, lat, lon, depth, time
+% sim       owtt, range, gvel, gvelstd
 
 clear; clc;
 
 %% load group velocity data
 dir_group_velocity = '~/.dropboxmit/icex_ipynb/gvels_by_node.mat';
 load(dir_group_velocity);
-gvels_topside = gvels_by_node.hydrohole;
-gvels_macrura = gvels_by_node.macrura;
+gvel_macrura = gvels_by_node.macrura;
 clear gvels_by_node
+
+% transform gvel_macrura structure, so it can be easily searched
+gvel_lbls = {'h1','h2','h3','h4'};
+num_gvel_lbls = length(gvel_lbls);
+
+for iGL = 1:num_gvel_lbls
+    temp = [gvel_macrura.(gvel_lbls{iGL}){:}];
+    gvel_macrura.(gvel_lbls{iGL}) = temp;
+end
+clear temp;
 
 %% load toby test data
 dir_toby_test = '~/.dropboxmit/icex_2020_mat/toby_test_sorted/*.mat';
@@ -51,40 +62,84 @@ for itt = 1
                 count = count + 1;
                 
                 % information from comms.(*).event
-                event(count).info.time  = h_convertTime(A.comms.(temp_id).event{iNE}.arr_time,0);
-                event(count).info.tstr  = h_convertTime(A.comms.(temp_id).event{iNE}.arr_time,1);
-                event(count).info.src   = A.comms.(temp_id).event{iNE}.src;
-                event(count).info.dest  = A.comms.(temp_id).event{iNE}.dest;
-                event(count).info.rec   = comm_ids(iNCI);
-                event(count).info.owtt  = A.comms.(temp_id).event{iNE}.travel_time;
+                event(count).tag.owtt       = A.comms.(temp_id).event{iNE}.travel_time;
+                event(count).tag.src        = A.comms.(temp_id).event{iNE}.src;
+                event(count).tag.dest       = A.comms.(temp_id).event{iNE}.dest;
+                event(count).tag.rec        = comm_ids(iNCI);
+                event(count).tag.name       = nameString;
+                event(count).tag.tstr       = h_convertTime(A.comms.(temp_id).event{iNE}.arr_time,1);
+                event(count).tag.time       = h_convertTime(A.comms.(temp_id).event{iNE}.arr_time,0);
                 
-                event(count).tx.id = A.comms.(temp_id).event{iNE}.src;
-               
-                % information from comms.(*).nav
-                
-                event(count).rx.id      = comm_ids(iNCI);
-                event(count).rx.name    = A.comms.(temp_id).nav{iNE}.name;
-                event(count).rx.x       = str2double(A.comms.(temp_id).nav{iNE}.x);
-                event(count).rx.y       = str2double(A.comms.(temp_id).nav{iNE}.y);
-                event(count).rx.z       = A.comms.(temp_id).nav{iNE}.z;
-                event(count).rx.lat     = str2double(A.comms.(temp_id).nav{iNE}.lat);
-                event(count).rx.lon     = str2double(A.comms.(temp_id).nav{iNE}.lon);
-                event(count).rx.depth   = str2double(A.comms.(temp_id).nav{iNE}.depth);
-                event(count).rx.time    = h_convertTime(A.comms.(temp_id).nav{iNE}.time,0);
+                event(count).tx.id          = A.comms.(temp_id).event{iNE}.src;
 
                 % information from comms.(*).src_nav
-                event(count).tx.name    = A.comms.(temp_id).src_nav{iNE}.name;
-                event(count).tx.x       = str2double(A.comms.(temp_id).src_nav{iNE}.x);
-                event(count).tx.y       = str2double(A.comms.(temp_id).src_nav{iNE}.y);
-                event(count).tx.z       = A.comms.(temp_id).src_nav{iNE}.z;
-                event(count).tx.lat     = str2double(A.comms.(temp_id).src_nav{iNE}.lat);
-                event(count).tx.lon     = str2double(A.comms.(temp_id).src_nav{iNE}.lon);
-                event(count).tx.depth   = A.comms.(temp_id).src_nav{iNE}.depth;
-                event(count).tx.time    = h_convertTime(A.comms.(temp_id).src_nav{iNE}.time,0);
+                event(count).tx.name        = A.comms.(temp_id).src_nav{iNE}.name;
+                event(count).tx.x           = smartI2D(A.comms.(temp_id).src_nav{iNE}.x);
+                event(count).tx.y           = smartI2D(A.comms.(temp_id).src_nav{iNE}.y);
+                event(count).tx.z           = A.comms.(temp_id).src_nav{iNE}.z;
+                event(count).tx.lat         = smartI2D(A.comms.(temp_id).src_nav{iNE}.lat);
+                event(count).tx.lon         = smartI2D(A.comms.(temp_id).src_nav{iNE}.lon);
+                event(count).tx.depth       = A.comms.(temp_id).src_nav{iNE}.depth;
+                event(count).tx.time        = h_convertTime(A.comms.(temp_id).src_nav{iNE}.time,0);
                 
-                % book keeping tag
-                event(count).tag = nameString;
+                % information from comms.(*).nav
+                event(count).rx.id          = comm_ids(iNCI);
+                event(count).rx.name        = A.comms.(temp_id).nav{iNE}.name;
+                event(count).rx.x           = smartI2D(A.comms.(temp_id).nav{iNE}.x);
+                event(count).rx.y           = smartI2D(A.comms.(temp_id).nav{iNE}.y);
+                event(count).rx.z           = A.comms.(temp_id).nav{iNE}.z;
+                event(count).rx.lat         = smartI2D(A.comms.(temp_id).nav{iNE}.lat);
+                event(count).rx.lon         = smartI2D(A.comms.(temp_id).nav{iNE}.lon);
+                event(count).rx.depth       = smartI2D(A.comms.(temp_id).nav{iNE}.depth);
+                event(count).rx.time        = h_convertTime(A.comms.(temp_id).nav{iNE}.time,0);
+                
+                % necessary variables for getting gvel information
+                % t0 = time, pointer = node
+                t0 = event(count).tag.time;
+                if comm_ids(iNCI) > 5
+                    node = event(count).rx.name;
+                elseif comm_ids(iNCI) == 4
+                    node = event(count).tx.name;
+                end
+        
+                % information from gvels_macrura - filter by src AND time
+                event(count).sim.gvelNode  = node; 
+                
+                time_array = h_convertTime(get_nested_val(gvel_macrura,node,'timestamp'),0);
+                [~,index] = min(abs(time_array-t0));
+                
+                event(count).sim.range     = gvel_macrura.(node)(index).range;
+                event(count).sim.delay     = gvel_macrura.(node)(index).delay;
+                event(count).sim.gvel      = gvel_macrura.(node)(index).group_velocity;
+                event(count).sim.gvelstd   = gvel_macrura.(node)(index).group_velocity_std;
+                event(count).sim.src       = gvel_macrura.(node)(index).source;
+                event(count).sim.rec       = gvel_macrura.(node)(index).receiver;
+                event(count).sim.time      = h_convertTime(gvel_macrura.(node)(index).timestamp,0);
             end       
         end 
     end
+end
+
+%% helper function : get_nested_val();
+% get a nested value as an array over all structs
+function [array] = get_nested_val(obj,lvl1,lvl2)
+stuff = [obj.(lvl1)];
+array = [stuff.(lvl2)];
+end
+
+%% helper function : smart_input2double();
+% not all data inputs are saved as strings... this checks before
+% converting, otherwise you get a NaN
+function [dbl] = smartI2D(input)
+
+% class(input)
+
+if isa(input,'char')
+   dbl = str2double(input);
+elseif isa(input,'double')
+   dbl = input;
+else
+   dbl = double(input);
+end
+
 end
