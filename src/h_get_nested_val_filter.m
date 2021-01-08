10d13125c509646a230740aa0fa3638b87a3dc83
eesh <eesh@mit.edu>
2021-01-08 16:36:11 -0500

    support for cell arrays of strings (lols)

--- src/h_get_nested_val_filter.m
+++ src/h_get_nested_val_filter.m
@@ -2,10 +2,21 @@
 % get a nested value as an array over all structs
 function [array] = h_get_nested_val_filter(obj,lvl1,lvl2,filter)
 stuff = [obj.(lvl1)];
-array = [stuff.(lvl2)];
 
-if exist('filter','var')
-    array = array(filter);
+temp = stuff(1).(lvl2);
+
+% for doubles
+if isa(temp,'double')
+    array = [stuff.(lvl2)];
+    if exist('filter','var')
+        array = array(filter);
+    end
+% for character arrays
+elseif isa(temp,'char')
+    array = {stuff.(lvl2)};
+    if exist('filter','var')
+        array = array{filter};
+    end
 end
 
 end
