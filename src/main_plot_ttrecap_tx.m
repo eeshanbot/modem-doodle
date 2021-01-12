bd87bdf3cea4d48ded1934ba826212b0cc23fe04
eesh <eesh@mit.edu>
2021-01-12 14:14:47 -0500

    remove ice plot, in main_plot_ice_drift.m

--- src/main_plot_ttrecap_tx.m
+++ src/main_plot_ttrecap_tx.m
@@ -105,96 +105,9 @@ axis equal
 legend(Lgd,LgdStr,'location','bestoutside')
 title(['Bird''s Eye View of Camp Seadragon, zs = ' num2str(zs) 'm'],'fontsize',20);
 
-%% figure 2 : x/y drift over experiment time
+%% figure 2 : ray trace differences
+
 % figure(2); clf;
-% [ixlgd,Lgd,LgdStr] = lgd_init();
-% 
-%     src_status  = boolean([]);
-%     src_date = [];
-% 
-% for ml = modem_labels
-%     ml = ml{1}; % cell array to character
-%     
-%     rec_xval = [];
-%     rec_yval = [];
-%     rec_date = [];
-%     
-%     for cfg = 1:2
-%         % ping rx
-%         tag_rx = CONFIG{cfg}.tag_rx;
-%         
-%         % grab data from this node
-%         index = find(strcmp(tag_rx,ml));
-%         
-%         rec_xval = [rec_xval CONFIG{cfg}.rx_x(index)];
-%         rec_yval = [rec_yval CONFIG{cfg}.rx_y(index)];
-%         
-%         rec_date  = [rec_date CONFIG{cfg}.data_time(index)];
-%         
-%         % ping tx
-%         tag_tx = CONFIG{cfg}.tag_tx;
-%         index = find(strcmp(tag_tx,ml));
-%         if ~isempty(index)
-%             rec_xval = [rec_xval CONFIG{cfg}.tx_x(index)];
-%             rec_yval = [rec_yval CONFIG{cfg}.tx_y(index)];
-%             rec_date = [rec_date CONFIG{cfg}.data_time(index)];
-%             
-%             % for patch --- experiment status
-%             src_status = [src_status CONFIG{cfg}.eof_bool(index)];
-%             src_date = [src_date CONFIG{cfg}.data_time(index)];
-%         end
-%     end
-%     
-%     % plot if exists
-%     if ~isempty(rec_xval)
-%         ixlgd = ixlgd + 1;
-%         % sort in time
-%         [rec_date,index] = sort(rec_date);
-%         rec_xval = rec_xval(index); rec_xval = rec_xval - rec_xval(1);
-%         rec_yval = rec_yval(index); rec_yval = rec_yval - rec_yval(1);
-%         
-%         k = 20 .* 1/60 .* 1/24; % X min moving average in days
-%         rec_xval_fit = movmean(rec_xval,k,'SamplePoints',rec_date);
-%         rec_yval_fit = movmean(rec_yval,k,'Samplepoints',rec_date);  
-%         
-%         % xval scatter
-%         subplot(2,1,1)
-%         hold on
-%         Lgd(ixlgd) = scatter(rec_date,rec_xval,markerSize/2,markerModemMap(ml),'filled','MarkerFaceAlpha',10.*alpha_color);
-%         LgdStr{ixlgd} = ml;
-%         plot(rec_date,rec_xval_fit,'-','linewidth',3,'color',[markerModemMap(ml) 10.*alpha_color]);
-%         datetick('x');
-%         grid on
-%         ylabel('x-direction [m]');
-%         axis tight
-% 
-%         % yval scatter
-%         subplot(2,1,2)
-%         hold on
-%         scatter(rec_date,rec_yval,markerSize/2,markerModemMap(ml),'filled','MarkerFaceAlpha',10.*alpha_color);
-%         plot(rec_date,rec_yval_fit,'-','linewidth',3,'color',[markerModemMap(ml) 10.*alpha_color]);
-%         datetick('x');
-%         grid on
-%         xlabel('time [hh:mm]');
-%         ylabel('y-direction [m]');
-%         axis tight
-%     end
-% end
-% 
-% subplot(2,1,1); 
-% hold off
-% title('Ice Floe Drift Recorded from Modem Buoy GPS');
-% plot_patch(src_date(src_status));
-% 
-% 
-% subplot(2,1,2);
-% hold off
-% legend(Lgd,LgdStr,'location','bestoutside');
-% plot_patch(src_date(src_status));
-
-%% figure 3 : ray trace differences
-
-% figure(3); clf;
 % [ixlgd,Lgd,LgdStr] = lgd_init();
 
 
@@ -204,26 +117,6 @@ function [ixlgd,Lgd,LgdStr] = lgd_init()
 ixlgd = 0;
 Lgd = [];
 LgdStr = {};
-end
-
-%% helper function : plot_patch
-function [] = plot_patch(patchTime)
-
-% get ybounds
-ybounds = ylim();
-
-patchTime = [patchTime(1) patchTime patchTime(end)];
-patchVal = ybounds(2).*ones(size(patchTime));
-patchVal(1) = ybounds(1);
-patchVal(end) = ybounds(1);
-p = patch(patchTime,patchVal,'w');
-p.FaceColor = [0.7 0.7 0.7];
-p.EdgeColor = 'none';
-p.FaceAlpha = .2;
-
-text(patchTime(end),ybounds(1)+1,'EOF   ','HorizontalAlignment','right','fontsize',12,'fontangle','italic')
-text(patchTime(end),ybounds(1)+1,'  BASEVAL','HorizontalAlignment','left','fontsize',12,'fontangle','italic')
-
 end
 
 
