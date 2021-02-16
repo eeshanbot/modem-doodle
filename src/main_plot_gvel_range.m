9b0d8d50d0b0d96e8605ad35390a8ebfd455cde0
eesh <eesh@mit.edu>
2021-02-16 00:30:18 -0500

    added plot to just show difference for data poitns that had a simulated gvel

--- src/main_plot_gvel_range.m
+++ src/main_plot_gvel_range.m
@@ -21,47 +21,68 @@ figure('Name','range-gvel-all','Renderer', 'painters', 'Position', [0 0 1280 720
 
 subplot(1,2,1)
 hold on
-%scatter(RECAP.sim_range,RECAP.sim_gvel,'o');
 for fRNX = filter_node
     index_rxn = strcmp(RECAP.tag_rx,fRNX{1});
+    
+    for fRXZ = filter_rxz
+        index_rxz = RECAP.rx_z == fRXZ;
         
-        for fRXZ = filter_rxz
-            index_rxz = RECAP.rx_z == fRXZ;
-            
-            index = boolean(index_rxn .* index_rxz);
-            
-            scatter(RECAP.sim_range(index),RECAP.sim_gvel(index),...
-                    markerSize,markerModemMap(fRNX{1}),markerShape(fRXZ),'filled','MarkerFaceAlpha',.05)
-        end
+        index = boolean(index_rxn .* index_rxz);
+        
+        scatter(RECAP.sim_range(index),RECAP.sim_gvel(index),...
+            markerSize,markerModemMap(fRNX{1}),markerShape(fRXZ),'filled','MarkerFaceAlpha',.1)
+    end
 end
 hold off
 grid on
 h_set_xy_bounds(RECAP.sim_range,RECAP.data_range,RECAP.sim_gvel,RECAP.data_range ./ RECAP.data_owtt);
-title('in situ prediction')
+title('In situ prediction')
 ylabel('group velocity [m/s]');
 xlabel('range [m]')
 
-
 subplot(1,2,2)
 hold on
+
 %scatter(RECAP.data_range,RECAP.data_range ./ RECAP.data_owtt,'o')
+
+index_gvel = ~isnan(RECAP.sim_gvel);
 for fRNX = filter_node
     index_rxn = strcmp(RECAP.tag_rx,fRNX{1});
+    
+    for fRXZ = filter_rxz
+        index_rxz = RECAP.rx_z == fRXZ;
         
-        for fRXZ = filter_rxz
-            index_rxz = RECAP.rx_z == fRXZ;
-            
-            index = boolean(index_rxn .* index_rxz);
-            
-            scatter(RECAP.data_range(index),RECAP.data_range(index) ./ RECAP.data_owtt(index),...
-                    markerSize,markerModemMap(fRNX{1}),markerShape(fRXZ),'filled','MarkerFaceAlpha',.05)
-        end
+        index = boolean(index_gvel .* index_rxn .* index_rxz);
+        
+        scatter(RECAP.data_range(index),RECAP.data_range(index) ./ RECAP.data_owtt(index),...
+            markerSize,markerModemMap(fRNX{1}),markerShape(fRXZ),'filled','MarkerFaceAlpha',.4)
+    end
 end
 hold off
 grid on
-h_set_xy_bounds(RECAP.sim_range,RECAP.data_range,RECAP.sim_gvel,RECAP.data_range ./ RECAP.data_owtt);
-title('post processing data')
+h_set_xy_bounds(RECAP.sim_range,RECAP.data_range,RECAP.sim_gvel(index_gvel),RECAP.data_range(index_gvel) ./ RECAP.data_owtt(index_gvel));
+title('Post-processing data')
 xlabel('range [m]')
 
+%% figure
+figure(2);
+
+index_gvel = ~isnan(RECAP.sim_gvel);
+for fRNX = filter_node
+    index_rxn = strcmp(RECAP.tag_rx,fRNX{1});
+    
+    for fRXZ = filter_rxz
+        index_rxz = RECAP.rx_z == fRXZ;
+        
+        index = boolean(index_gvel .* index_rxn .* index_rxz);
+        hold on
+        scatter(RECAP.data_range(index) ./ RECAP.data_owtt(index),...
+            RECAP.sim_gvel(index),...
+            markerSize,markerModemMap(fRNX{1}),markerShape(fRXZ),'filled','MarkerFaceAlpha',.2);
+        hold off
+    end
+end
+
+grid on
 
 
