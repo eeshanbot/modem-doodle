9f13719158563bee4dde19cb2c7a1fd482e69876
eesh <eesh@mit.edu>
2021-03-01 14:26:07 -0500

    makes raytrace separately

--- src/main_plot_raytrace.m
+++ src/main_plot_raytrace.m
@@ -11,7 +11,7 @@ charcoalGray = [0.6 0.6 0.6];
 alphaColor   = .035;
 
 % depth_switch = [20 30 90];
-zs = 90;
+zs = 20;
 
 %% load important things
 
@@ -36,7 +36,7 @@ EEOF = h_unpack_experiment(A.experiment);
 
 % create cell array of structure
 BASE.title = '{\it Baseval}';
-EEOF.title = '{\it EOF}';
+EEOF.title = '{\it Chosen EOF}';
 CONFIG = {BASE EEOF};
 clear BASE EEOF;
 
@@ -61,34 +61,155 @@ end
 
 %% figure : ray trace
 
-figure('Name','ray trace','Renderer', 'painters', 'Position', [10 10 1700 1000]); clf;
+% figure('Name','ray trace','Renderer', 'painters', 'Position', [10 10 1700 1000]); clf;
+% 
+% % max plot depth
+% plotDepth = 300;
+% 
+% for cfg = 1:2
+%     
+%     % sound speed plot
+%     subplot(2,10,[cfg*10-9 cfg*10-8])
+%     plot(CONFIG{cfg}.ssp_estimate,CONFIG{cfg}.ssp_depth,'k.-','markersize',15)
+%     set(gca,'ydir','reverse')
+%     grid on
+%     ylim([0 plotDepth]);
+%     ylabel('z [m]');
+%     title([CONFIG{cfg}.title ' ssp']);
+%     if cfg == 2
+%         xlabel('c [m/s]');
+%     end
+%     xlim([1432 1448])
+%     
+%     % plot rays
+%     subplot(2,10,[cfg*10-7 cfg*10]);
+%     hold on
+%     num_rays = numel(CONFIG{cfg}.raytraceR);
+%     for nrz = 1:num_rays
+%         plot(CONFIG{cfg}.raytraceR{nrz},CONFIG{cfg}.raytraceZ{nrz},'color',[charcoalGray 0.1],'handlevisibility','off');
+%     end
+%     
+%     % plot eigenrays
+%     num_eigentable = numel(eigentable);
+%     for ne = 1:num_eigentable
+%         eof_status = double(eigentable{ne}.eof_status+1);
+%         tx_z = double(eigentable{ne}.tx_z);
+%         
+%         if (eof_status == cfg && tx_z == zs)
+%             
+%             if ~strcmp(eigentable{ne}.ray,'None')
+%             
+%                 plot(eigentable{ne}.ray.r,eigentable{ne}.ray.z,...
+%                     'color',[markerModemMap(eigentable{ne}.rx_node) 0.5],'linewidth',2,'handlevisibility','off')
+%             end
+%         end
+%     end
+%     
+%     % decorate plot
+%     hold off
+%     title(sprintf('%s rays and eigenrays, zs=%u m',CONFIG{cfg}.title,zs));
+%     yticks(0:50:300);
+%     yticklabels([]);
+%     axis tight
+%     xlim([0 maxRange]);
+%     ylim([0 plotDepth])
+%     set(gca,'ydir','reverse')
+%     box on
+%     
+%     % xlabel if bottom
+%     if cfg == 2
+%         xlabel('range [m]');
+%     end
+%     
+%     % plot source
+%     hold on
+%     scatter(0,zs,markerSize,'k','s','linewidth',2);
+%     
+%     % plot modem shapes
+%     for node = CONFIG{cfg}.unique_rx
+%         node = node{1}; % change from cell to char
+%         for imd = modem_rx_depth
+%             index = find(strcmp(CONFIG{cfg}.tag_rx,node) & CONFIG{cfg}.rx_z == imd);
+%             if ~isempty(index)
+%                 scatter(CONFIG{cfg}.data_range(index),CONFIG{cfg}.rx_z(index),...
+%                     markerSize,markerModemMap(node),markerShape(imd),'filled');
+%                 
+%                 % check by TX nodes 
+%                 tx_nodes = CONFIG{cfg}.tag_tx(index);
+%                 unq_tx_nodes = unique(tx_nodes);
+%                 
+%                 % report amount at each rx modem node
+%                 for utn = unq_tx_nodes
+%                     subindex = find((CONFIG{cfg}.rx_z == imd) & (strcmp(CONFIG{cfg}.tag_tx,utn{1})) & (strcmp(CONFIG{cfg}.tag_rx,node)));
+%                     text(mean(CONFIG{cfg}.data_range(subindex)),imd+14,num2str(numel(subindex)),...
+%                         'HorizontalAlignment','center','VerticalAlignment','top','fontsize',12,'color',markerModemMap(node))
+%                 end
+%             end
+%         end
+%     end
+%     
+%     % hold off for subplot
+%     hold off
+% end
+% 
+% %% make legend - manual is easier
+% load p_legendDetails.mat
+% subplot(2,10,[cfg*10-7 cfg*10]);
+% hold on
+% for node = modem_labels
+%     node = node{1};
+%     
+%     index1 = find(strcmp(CONFIG{1}.tag_rx,node));
+%     index2 = find(strcmp(CONFIG{2}.tag_rx,node));
+%     index = union(index1,index2);
+%     
+%     if ~isempty(index)
+%         % get tx depths
+%         zvals = [CONFIG{1}.rx_z(index1) CONFIG{2}.rx_z(index2)];
+%         unq_zvals = unique(zvals);
+%         
+%         for uz = unq_zvals
+%             ixlgd = ixlgd + 1;
+%             Lgd(ixlgd) = scatter(NaN,NaN,markerSize,markerModemMap(node),markerShape(uz),'filled');
+%             LgdStr{ixlgd} = [num2str(uz) ' m | ' node];
+%         end
+%     end
+% end
+% 
+% lg = legend(Lgd,LgdStr,'location','SouthWest','fontsize',12);
+% title(lg,'rx nodes');
+% 
+% %% export
+% h_printThesisPNG(sprintf('zs%u-raytrace.png',zs))
+
+%% make plot of just the EOF sound speed
+% figure : ray trace
+
+figure('Name','raytrace-single','Renderer', 'painters', 'Position', [10 10 1700 600]); clf;
 
 % max plot depth
 plotDepth = 300;
 
-for cfg = 1:2
-    
-    % sound speed plot
-    subplot(2,10,[cfg*10-9 cfg*10-8])
-    plot(CONFIG{cfg}.ssp_estimate,CONFIG{cfg}.ssp_depth,'k.-','markersize',15)
-    set(gca,'ydir','reverse')
-    grid on
-    ylim([0 plotDepth]);
-    ylabel('z [m]');
-    title([CONFIG{cfg}.title ' ssp']);
-    if cfg == 2
-        xlabel('c [m/s]');
-    end
-    xlim([1432 1448])
+for cfg = 2
+    hold on
+%     % sound speed plot
+%     subplot(1,10,[1 2])
+%     plot(CONFIG{cfg}.ssp_estimate,CONFIG{cfg}.ssp_depth,'k.-','markersize',15)
+%     set(gca,'ydir','reverse')
+%     grid on
+%     ylim([0 plotDepth]);
+%     ylabel('z [m]');
+%     title([CONFIG{cfg}.title ' ssp']);
+%     if cfg == 2
+%         xlabel('c [m/s]');
+%     end
+%     xlim([1432 1456])
     
     % plot rays
-    subplot(2,10,[cfg*10-7 cfg*10]);
-    hold on
     num_rays = numel(CONFIG{cfg}.raytraceR);
     for nrz = 1:num_rays
         plot(CONFIG{cfg}.raytraceR{nrz},CONFIG{cfg}.raytraceZ{nrz},'color',[charcoalGray 0.1],'handlevisibility','off');
     end
-    
     % plot eigenrays
     num_eigentable = numel(eigentable);
     for ne = 1:num_eigentable
@@ -106,15 +227,14 @@ for cfg = 1:2
     end
     
     % decorate plot
-    hold off
-    title(sprintf('%s rays and eigenrays, zs=%u m',CONFIG{cfg}.title,zs));
+    title(sprintf('%s rays and eigenrays, source depth = %u m',CONFIG{cfg}.title,zs));
     yticks(0:50:300);
-    yticklabels([]);
     axis tight
     xlim([0 maxRange]);
     ylim([0 plotDepth])
     set(gca,'ydir','reverse')
     box on
+    ylabel('depth [m]');
     
     % xlabel if bottom
     if cfg == 2
@@ -122,7 +242,6 @@ for cfg = 1:2
     end
     
     % plot source
-    hold on
     scatter(0,zs,markerSize,'k','s','linewidth',2);
     
     % plot modem shapes
@@ -154,7 +273,6 @@ end
 
 %% make legend - manual is easier
 load p_legendDetails.mat
-subplot(2,10,[cfg*10-7 cfg*10]);
 hold on
 for node = modem_labels
     node = node{1};
@@ -178,6 +296,3 @@ end
 
 lg = legend(Lgd,LgdStr,'location','SouthWest','fontsize',12);
 title(lg,'rx nodes');
-
-%% export
-h_printThesisPNG(sprintf('zs%u-raytrace.png',zs))
