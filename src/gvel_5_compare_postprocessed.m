f5ca2f82083310468f4d83baeba642679bcfcc8e
eesh <eesh@mit.edu>
2021-05-14 22:54:30 -0400

    output for table, no need for plot

--- src/gvel_5_compare_postprocessed.m
+++ src/gvel_5_compare_postprocessed.m
@@ -2,26 +2,31 @@
 % compares range anomaly performance between post processing of old and new
 % algorithms
 
-clear; clc; close all;
+clear; clc;
 
 %% load in situ data
 DATA = readtable('./bellhop-gvel-gridded/gveltable.csv');
 A = load('./data-prep/tobytest-recap-full.mat'); % loads "event"
 RECAP = h_unpack_experiment(A.event);
 
+DATA.simGvel(isnan(DATA.simGvel)) = 0;
+
 % remove crazy 11 second event, event that is nominally 1.58* seconds
-indBad1 = DATA.owtt > 6;
-indBad2 = strcmp(DATA.rxNode,'East') & DATA.owtt > 1.55;
-indBad = indBad1 | indBad2;
+indBad1 = find(DATA.owtt > 3);
+indBad2 = find(strcmp(DATA.rxNode,'East') & DATA.owtt > 1.55);
+indBad = union(indBad1,indBad2);
 
 % 1.587 events, had clock errors + Bellhop can't resolve these
-DATA.simGvel(indBad) = -1000;
+DATA.simGvel(indBad) = NaN;
 % only simGvel
 
-indValid = ~indBad;
-%indValid = ~isnan(DATA.simGvel);
+indValid = ~isnan(DATA.simGvel);
+
 % calculate RangeAnomaly
 DATA.rangeAnomaly = DATA.owtt .* DATA.simGvel - DATA.recRange;
+
+% of interest
+roi = 1215:1224;
 
 %% load post-processing, new algorithm
 listing = dir('./bellhop-gvel-gridded/csv_arr/*gridded.csv');
@@ -72,7 +77,7 @@ end
 % 5 = hycom
 
 %% figure --- baseval
-figure('name','compare-method-baseval','renderer','painters','position',[108 108 1000 900]);
+%figure('name','compare-method-baseval','renderer','painters','position',[108 108 1000 900]);
 
 index = indValid;
 
@@ -83,15 +88,8 @@ baseval.zs = DATA.sourceDepth(index);
 baseval.numBounces = SIM_NEW{3}.numBounces(index);
 baseval.F = h_cross_plot(baseval.xVal,baseval.yVal,baseval.zs,baseval.numBounces);
 
-% title
-bigTitle = '\fontsize{18} Comparison of range estimation error in post-processing';
-smallTitle = sprintf('\\fontsize{14}\\rm SSP = mean of EOF set, N = %u events',sum(index));
-title({bigTitle; smallTitle});
-
-%h_printThesisPNG('compare-baseval-post');
-
 %% figure --- EOF
-figure('name','compare-method-eeof','renderer','painters','position',[108 108 1000 900]);
+%figure('name','compare-method-eeof','renderer','painters','position',[108 108 1000 900]);
 
 % only simGvel
 index = indValid;
@@ -103,15 +101,8 @@ eeof.zs = DATA.sourceDepth(index);
 eeof.numBounces = SIM_NEW{4}.numBounces(index);
 eeof.F = h_cross_plot(eeof.xVal,eeof.yVal,eeof.zs,eeof.numBounces);
 
-% title
-bigTitle = '\fontsize{18} Comparison of range estimation error in post-processing';
-smallTitle = sprintf('\\fontsize{14}\\rm SSP = chosen weights, N = %u events',sum(index));
-title({bigTitle; smallTitle});
- 
-% %h_printThesisPNG('compare-eof-post');
-
 %% figure --- HYCOM
-figure('name','compare-method-hycom','renderer','painters','position',[108 108 1000 900])
+%figure('name','compare-method-hycom','renderer','painters','position',[108 108 1000 900])
 
 % plot
 hycom.xVal = SIM_OLD{5}.rangeAnomaly(indValid);
@@ -120,84 +111,18 @@ hycom.zs   = DATA.sourceDepth(indValid);
 hycom.numBounces = SIM_NEW{5}.numBounces(indValid);
 hycom.F = h_cross_plot(hycom.xVal,hycom.yVal,hycom.zs,hycom.numBounces);
 
-% title
-bigTitle = '\fontsize{18} Comparison of range estimation error in post-processing';
-smallTitle = sprintf('\\fontsize{14}\\rm SSP = HYCOM, N = %u events',sum(indValid));
-title({bigTitle; smallTitle});
-
-%h_printThesisPNG('compare-hycom-post');
-
-
 %% figure helper function
 function [F] = h_cross_plot(xVal,yVal,zs,numBounces)
 
-% marker shape & color
-shapeBounce = {'o','x','s','^','d'};
-colorDepth = containers.Map([20 30 90],{[70 240 240]./256,[0 130 200]./256,[0 0 128]./256});
+xVal = abs(xVal);
+yVal = abs(yVal);
 
-maxVal(1) = max(xVal);
-maxVal(2) = max(yVal);
-maxVal = max(maxVal(:));
+F.Xquant = quantile(xVal,[0 0.25 0.5 0.75 1]);
+F.Xmean = mean(xVal);
+F.Xstd = std(xVal);
 
-% add patch
-hold on
-p = patch([-maxVal 0 -maxVal maxVal 0 maxVal],[-maxVal 0 maxVal maxVal 0 -maxVal],'w','handlevisibility','off');
-p.FaceColor = [0.7 0.7 0.7];
-p.FaceAlpha = .3;
-p.EdgeColor = 'none';
-hold off
-
-% scatter points
-hold on
-for k = 1:numel(zs)
-    scatter(xVal(k),yVal(k),...
-        150,colorDepth(zs(k)),shapeBounce{numBounces(k)+1},'linewidth',2,'markeredgealpha',0.6,'handlevisibility','off');
-end
-hold off
-
-% performance metrics
-F.dataMean = mean(abs(xVal));
-F.simMean = mean(abs(yVal));
-F.dataMedian = median(abs(xVal));
-F.simMedian = median(abs(yVal));
-F.dataStd = std(xVal);
-F.simStd = std(yVal);
-F.eff = sum(abs(yVal) <= abs(xVal))./numel(xVal);
-F.rms = rms(abs(xVal)-abs(yVal));
-
-% add grid
-grid on
-
-% add text to explain gray box
-buff = maxVal/8;
-text(-maxVal+buff,maxVal-buff,'more accurate','verticalalignment','top','rotation',-45,'fontsize',11);
-text(-maxVal+buff,maxVal-buff,'less accurate than in-situ algorithm','verticalalignment','bottom','rotation',-45,'fontsize',11);
-
-% make xticks and yticks equal
-axis tight
-axis square
-xticks(yticks);
-
-% make plot look nice
-xlabel({'range error [m]','\it{minimal bounce criteria}'});
-ylabel({'range error [m]','\it{nearest bounce criteria}'});
-set(gca,'fontsize',14);
-
-% add legend
-hold on
-for s = [20 30 90]
-    plot(NaN,NaN,'color',colorDepth(s),'linewidth',6);
-end
-
-plot(NaN,NaN,'w');
-
-for r = 1:5
-    plot(NaN,NaN,shapeBounce{r},'color','k')
-end
-hold off
-lgdstr = {'20 m','30 m','90 m','','direct path','1 bounce','2 bounces','3 bounces'};
-
-lg1 = legend(lgdstr,'location','south','NumColumns',2,'fontsize',11);
-title(lg1,'   source depth & multipath structure');
+F.Yquant = quantile(yVal,[0 0.25 0.5 0.75 1]);
+F.Ymean = mean(yVal);
+F.Ystd = std(yVal);
 
 end
