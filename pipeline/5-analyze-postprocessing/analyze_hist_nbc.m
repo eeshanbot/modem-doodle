0c3c27cbb14dbf34839ff9f5ace74b1f060f7d7f
eesh <eesh@mit.edu>
2022-02-15 16:12:31 -0500

    pipeline5, 4 plots in 1 script

--- pipeline/5-analyze-postprocessing/analyze_hist_nbc.m
+++ pipeline/5-analyze-postprocessing/analyze_hist_nbc.m
@@ -1,64 +1,45 @@
-%% gvel_3_simAllEvents
-
 %% prep workspace
 clear; clc; close all;
 
-% load data
-DATA = readtable('../bellhop-gvel-gridded/gveltable.csv');
-% only simGvel
-DATA.gvel = DATA.recRange ./ DATA.owtt;
+%% load data
 
-DATA.simGvel(isnan(DATA.simGvel)) = 0;
+[DATA,INDEX] = h_unpack_bellhop('../bellhop-gvel-gridded/gveltable.csv');
 
-% remove crazy 11 second event, event that is nominally 1.58* seconds
-indBad1 = find(DATA.owtt > 4);
-indBad2 = find(strcmp(DATA.rxNode,'East') & DATA.owtt > 1.55);
-indBad3 = find(strcmp(DATA.rxNode,'Camp'));
-indBad = union(indBad1,indBad2);
-indBad = union(indBad,indBad3);
-
-% 1.587 events, had clock errors + Bellhop can't resolve these
-DATA.simGvel(indBad) = NaN;
-% only simGvel
-
-indValid = ~isnan(DATA.simGvel);
-
-% calculate RangeAnomaly
-DATA.rangeAnomaly = DATA.owtt .* DATA.simGvel - DATA.recRange;
 %% load simulation
 listing = dir('../bellhop-gvel-gridded/csv_arr/*gridded.csv');
+[T,colorSet] = h_get_nbc(listing,DATA,INDEX);
 
-for k = 1:numel(listing)
-    T0 = readtable([listing(k).folder '/' listing(k).name]);
-    T0.index = T0.index + 1;
-    b = split(listing(k).name,'.');
-    tName{k} = b{1};
-    
-    % assign gvel for each index by closest time comparison
-    for j = 1:numel(T0.index)
-        delay = DATA.owtt(j);
-        tableDelay = table2array(T0(j,2:6));
-        [~,here] = min(abs(tableDelay - delay));
-        T0.gvel(j) = DATA.recRange(j)./tableDelay(here);
-        T0.owtt(j) = tableDelay(here);
-        T0.numBounces(j) = here-1;
-        if sum(j == indBad) == 1
-            T0.gvel(j) = NaN;
-        end
-    end
-    T{k} = T0;
-end
-
-% 1 = artifact-baseval
-% 2 = artifact-eeof
-% 3 = fixed-baseval
-% 4 = fixed-eeof
-% 5 = hycom
+% for k = 1:numel(listing)
+%     T0 = readtable([listing(k).folder '/' listing(k).name]);
+%     T0.index = T0.index + 1;
+%     b = split(listing(k).name,'.');
+%     tName{k} = b{1};
+%     
+%     % assign gvel for each index by closest time comparison
+%     for j = 1:numel(T0.index)
+%         delay = DATA.owtt(j);
+%         tableDelay = table2array(T0(j,2:6));
+%         [~,here] = min(abs(tableDelay - delay));
+%         T0.gvel(j) = DATA.recRange(j)./tableDelay(here);
+%         T0.owtt(j) = tableDelay(here);
+%         T0.numBounces(j) = here-1;
+%         if sum(j == INDEX.bad) == 1
+%             T0.gvel(j) = NaN;
+%         end
+%     end
+%     T{k} = T0;
+% end
+% 
+% % 1 = artifact-baseval
+% % 2 = artifact-eeof
+% % 3 = fixed-baseval
+% % 4 = fixed-eeof
+% % 5 = hycom
 
 % load modem marker information
 load p_modemMarkerDetails
 
-colorSet = {[0 0 0],[0 0 0],[232, 153, 35]./256,[0 85 135]./256,[152 134 117]./256};
+ % colorSet = {[0 0 0],[0 0 0],[232, 153, 35]./256,[0 85 135]./256,[152 134 117]./256};
 
 %% plot all data group velocity
 
@@ -77,7 +58,7 @@ for zs = [20 30 90]
         count = count + 1;
         nexttile;
         
-        index = logical(index1.*index2.*indValid);
+        index = logical(index1.*index2.*INDEX.valid);
         
         if sum(index)>=1
             % plot
@@ -174,7 +155,7 @@ for zs = [20 30 90]
         count = count + 1;
         nexttile;
         
-        index = logical(index1.*index2.*indValid);
+        index = logical(index1.*index2.*INDEX.valid);
         
         if sum(index)>=1
             % plot
